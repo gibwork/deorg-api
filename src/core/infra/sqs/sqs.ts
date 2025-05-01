d964fe353549d6711ce213a4ccce85141e026bee
Redfox <vitor_pereira.2013@hotmail.com>
2025-05-01 11:50:43 -0300

    refactor: remove hardcoded queue exports and producers from SQS configuration

--- src/core/infra/sqs/sqs.ts
+++ src/core/infra/sqs/sqs.ts
@@ -5,10 +5,6 @@ import {
 import * as AWS from 'aws-sdk';
 import process from 'node:process';
 
-export const TASK_CREATED_QUEUE = String(process.env.TASK_CREATED_QUEUE);
-export const TASK_PAID_QUEUE = String(process.env.TASK_PAID_QUEUE);
-export const EMAIL_QUEUE = String(process.env.EMAIL_QUEUE);
-
 AWS.config.update({
   region: process.env.AWS_REGION
 });
@@ -22,21 +18,3 @@ export const queues: QueueConfiguration = {
   producers: [],
   consumers: []
 };
-
-queues.producers.push({
-  name: `${TASK_CREATED_QUEUE}`,
-  queueUrl: `${process.env.SQS_BASE_URL}${TASK_CREATED_QUEUE}`,
-  region: process.env.AWS_REGION
-});
-
-queues.producers.push({
-  name: `${TASK_PAID_QUEUE}`,
-  queueUrl: `${process.env.SQS_BASE_URL}${TASK_PAID_QUEUE}`,
-  region: process.env.AWS_REGION
-});
-
-queues.producers.push({
-  name: `${EMAIL_QUEUE}`,
-  queueUrl: `${process.env.SQS_BASE_URL}${EMAIL_QUEUE}`,
-  region: process.env.AWS_REGION
-});
