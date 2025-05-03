9f4743eaeb84b28a60b685759b6f1a0ba8014353
Redfox <vitor_pereira.2013@hotmail.com>
2025-05-03 13:01:37 -0300

    feat: enhance organization membership check by integrating organization member service

--- src/domains/organizations/usecases/check-organization-membership.usecase.ts
+++ src/domains/organizations/usecases/check-organization-membership.usecase.ts
@@ -3,12 +3,13 @@ import { OrganizationService } from '../services/organization.service';
 import { UserEntity } from '@domains/users/entities/user.entity';
 import { Network } from '@utils/network';
 import { GetUserBalanceUseCase } from '@domains/users/usecases/get-user-balance.usecase';
-
+import { OrganizationMemberService } from '../services/organization-member.service';
 @Injectable()
 export class CheckOrganizationMembershipUsecase {
   constructor(
     private readonly organizationService: OrganizationService,
-    private readonly getUserBalanceUseCase: GetUserBalanceUseCase
+    private readonly getUserBalanceUseCase: GetUserBalanceUseCase,
+    private readonly organizationMemberService: OrganizationMemberService
   ) {}
 
   async execute(organizationId: string, user: UserEntity) {
@@ -20,6 +21,9 @@ export class CheckOrganizationMembershipUsecase {
     if (!organization) {
       throw new NotFoundException('Organization not found');
     }
+    const isMember = await this.organizationMemberService.findOne({
+      where: { organizationId, userId: user.id }
+    });
 
     const userBalance = await this.getUserBalanceUseCase.execute(
       user.walletAddress,
@@ -43,6 +47,7 @@ export class CheckOrganizationMembershipUsecase {
 
     return {
       allowed: true,
+      isMember: !!isMember,
       message: 'User has the required token amount'
     };
   }
