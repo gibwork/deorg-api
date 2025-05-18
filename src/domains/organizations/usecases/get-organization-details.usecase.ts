0413003c4b3cc9cdf1c79f7406183e61c51804de
Redfox <vitor_pereira.2013@hotmail.com>
2025-05-18 16:23:43 -0300

    feat: integrate HeliusService for treasury balance token info retrieval in GetOrganizationDetailsUsecase

--- src/domains/organizations/usecases/get-organization-details.usecase.ts
+++ src/domains/organizations/usecases/get-organization-details.usecase.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { OrganizationService } from '../services/organization.service';
 import { VotingProgramService } from '@core/services/voting-program/voting-program.service';
+import { HeliusService } from '@core/services/helius/helius.service';
+
 @Injectable()
 export class GetOrganizationDetailsUsecase {
   constructor(
     private readonly organizationService: OrganizationService,
-    private readonly votingProgramService: VotingProgramService
+    private readonly votingProgramService: VotingProgramService,
+    private readonly heliusService: HeliusService
   ) {}
 
   async execute(accountAddress: string) {
@@ -22,6 +25,19 @@ export class GetOrganizationDetailsUsecase {
         }
       }
     });
+
+    const treasuryBalances: any[] = [];
+
+    for (const balance of onChainOrganization.treasuryBalances) {
+      const token = await this.heliusService.getDevnetTokenInfo(balance.mint);
+
+      treasuryBalances.push({
+        ...balance,
+        token
+      });
+    }
+
+    onChainOrganization.treasuryBalances = treasuryBalances;
 
     return { ...onChainOrganization, ...organization };
   }
